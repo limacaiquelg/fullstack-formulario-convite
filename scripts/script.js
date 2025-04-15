26801174478ec9777f96cb32227ed497a5be362f
Caique Lima <limacaiquelg@gmail.com>
2025-04-15 12:43:46 -0300

    creating contact fieldset + adding error messages to the fields

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 const fileInput = document.getElementById('cover-photo');
 const fileName = document.getElementsByClassName('filename')[0];
-const errorMessage = document.getElementsByClassName('error-message')[0];
+const errorMessage = document.getElementById('file-input-error');
 
 fileInput.addEventListener('change', function() {
   if (fileInput.files.length > 0) {
